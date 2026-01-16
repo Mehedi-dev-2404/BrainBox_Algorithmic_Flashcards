55a837ef21f0603df8432bb944e450d0b9aa5c94
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-01-16 17:57:45 +0000

    Add interactive menu and card addition to brainbox.py
    Introduced an interactive menu for adding flashcards, practicing, viewing stats, and exiting. Implemented the add_card function to allow users to input new flashcards, which are then saved to flashcards.json.

--- brainbox.py
+++ brainbox.py
@@ -2,11 +2,10 @@ import json
 import os
 
 flash_file = 'flashcards.json'
-data = list()
+
 try:
     with open(flash_file, 'r') as f:
         content = f.read()
-        print("file is there")
 except FileNotFoundError:
     with open('flashcards.json', "w") as f:
         f.write("[]")
@@ -20,6 +19,37 @@ def save(data):
     with open(flash_file, "w") as f:
         json.dump(data, f)
 
-cards = load()
-print(cards)
-print(type(cards))
+def add_card(data):
+    load_data = load()
+    question = input("Question: ")
+    answer = input("Answer: ")
+    card_data = {
+        "question": question, 
+                 "answer": answer, 
+                 "level" : 1, 
+                 "next_review" : "today"
+                 }
+    data.append(card_data)
+    save(data)
+
+data = load()
+
+while True:
+    print("""
+1. Add Card
+2. Practice
+3. View Stats
+4. Exit""" )
+    operation = int(input("Enter which operation do you want to perform (1-4): "))
+
+    if operation == 1:
+        response = add_card(data)
+    elif operation == 2:
+        pass
+    elif operation == 3:
+        pass
+    elif operation == 4:
+        pass
+        break
+
+print(data)
